cabeb34cb6e5da20c01e0677ba83aeb975a73b21
= <=>
2020-06-04 15:05:24 +0100

    minor edit

--- _js/register-sw.js
+++ _js/register-sw.js
@@ -4,6 +4,7 @@ const SERVICE_WORKER = './sw.js';
 if ('serviceWorker' in navigator) {
     console.log("Registering...");
     navigator.serviceWorker.register(SERVICE_WORKER).then(function (registration) {
+        //console.log(registration)
         console.log("Registration of " + SERVICE_WORKER + " was successful.");
     }).catch(function (error) {
         //test by using xsw.js
